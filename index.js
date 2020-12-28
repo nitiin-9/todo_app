e4e2959138f423033cc5ae29995f492809054f96
nithin <nithink782@gmail.com>
2020-12-28 20:22:11 +0530

    initial commit setting server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app =express();
+const port = 4000; 
+
+
+
+app.listen(port,function(err){
+  if(err) {
+      console.log(`error in running the server ${err}`);
+  }
+  console.log(`server is running on port:${port}`);
+
+})
+
